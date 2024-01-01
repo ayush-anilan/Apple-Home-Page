333023df79a7c5ac46854a7970862fab2f9ab29e
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-01-01 23:23:45 +0530

    final

--- script.js
+++ script.js
@@ -5,18 +5,3 @@ hamBurger.addEventListener("click", () => {
   hamBurger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
-
-function toggleDropdown() {
-  var dropdown = document.querySelector(".col-heading");
-  var colDetails = document.querySelector(".col-details");
-
-  // Toggle the 'active' class on col-details
-  colDetails.classList.toggle("active");
-
-  // If dropdown is already displayed, hide it; otherwise, show it
-  if (colDetails.classList.contains("active")) {
-    dropdown.style.display = "block";
-  } else {
-    dropdown.style.display = "none";
-  }
-}
